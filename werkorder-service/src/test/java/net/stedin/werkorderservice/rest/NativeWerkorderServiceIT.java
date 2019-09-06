671e8d10e43ccb90541f6ffa7bf61de0379d886f
Serkan80 <seskici2009@gmail.com>
2019-09-06 19:04:47 +0200

    removed unit tests

--- werkorder-service/src/test/java/net/stedin/werkorderservice/rest/NativeWerkorderServiceIT.java
+++
@@ -1,9 +0,0 @@
-package net.stedin.werkorderservice.rest;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeWerkorderServiceIT extends WerkorderServiceTest {
-
-    // Execute the same tests but in native mode.
-}
