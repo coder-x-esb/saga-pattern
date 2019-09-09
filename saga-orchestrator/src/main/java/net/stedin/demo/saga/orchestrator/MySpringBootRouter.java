be77ad8a467caa6364bde401b63d75359cb07207
Alvin Kwekel <alvin.kwekel@stedin.net>
2019-09-09 20:00:26 +0200

    Camel orchestrator draft

---
+++ saga-orchestrator/src/main/java/net/stedin/demo/saga/orchestrator/MySpringBootRouter.java
@@ -0,0 +1,22 @@
+package net.stedin.demo.saga.orchestrator;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MySpringBootRouter extends RouteBuilder {
+
+    @Override
+    public void configure() {
+        from("timer:hello?period=5s").routeId("timer")
+            .saga()
+                .setHeader("id", header(Exchange.TIMER_COUNTER))
+                .setHeader(Exchange.HTTP_METHOD, constant("POST"))
+                .log("Executing saga #${header.id}")
+                .to("http4://localhost:9081/medewerkers?httpMethod=GET")
+                .setBody("${header}")
+                .to("http4://localhost:8080/api/flight/buy");;
+    }
+
+}
