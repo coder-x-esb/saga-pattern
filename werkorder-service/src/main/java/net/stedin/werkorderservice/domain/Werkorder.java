09c7a3579292a054f7de8e96a7bd36c7d2459ce9
coder-x-esb <serkan.eskici@stedin.net>
2019-09-10 15:11:47 +0200

    fixed json serialization of unwanted fields

--- werkorder-service/src/main/java/net/stedin/werkorderservice/domain/Werkorder.java
+++ werkorder-service/src/main/java/net/stedin/werkorderservice/domain/Werkorder.java
@@ -2,6 +2,7 @@ package net.stedin.werkorderservice.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
@@ -17,6 +18,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
@@ -28,21 +31,36 @@ public class Werkorder extends PanacheEntityBase {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "werkorder_SEQ")
     public Long id;
 
+    @NotNull
     public Long klantId;
+
+    @NotNull
     public Long monteurId;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
     public WerkorderStatus status;
+
+    @NotNull
     public String aangemaaktDoor;
 
+    @NotNull
     @JsonFormat(shape = Shape.STRING, pattern = "dd-MM-yyyy")
     @JsonSerialize(using = LocalDateSerializer.class)
     @JsonDeserialize(using = LocalDateDeserializer.class)
     public LocalDate aanmaakDatum;
+
+    @NotBlank
     public String omschrijving;
     public String commentaar;
 
     @Embedded
     public Adres adres;
+
+    @Override
+    @JsonIgnore
+    public boolean isPersistent() {
+        return super.isPersistent();
+    }
 }
 
