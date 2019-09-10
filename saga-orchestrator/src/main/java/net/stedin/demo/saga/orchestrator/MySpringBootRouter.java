568c557c1463cf7cd82434148d3e48f13978b4b3
coder-x-esb <serkan.eskici@stedin.net>
2019-09-10 12:11:05 +0200

    fixed compilation errors & update ignore file

--- saga-orchestrator/src/main/java/net/stedin/demo/saga/orchestrator/MySpringBootRouter.java
+++ saga-orchestrator/src/main/java/net/stedin/demo/saga/orchestrator/MySpringBootRouter.java
@@ -11,12 +11,11 @@ public class MySpringBootRouter extends RouteBuilder {
     public void configure() {
         from("timer:hello?period=5s").routeId("timer")
             .saga()
-                .setHeader("id", header(Exchange.TIMER_COUNTER))
-                .setHeader(Exchange.HTTP_METHOD, constant("POST"))
-                .log("Executing saga #${header.id}")
-                .to("http4://localhost:9081/medewerkers?httpMethod=GET")
-                .setBody("${header}")
-                .to("http4://localhost:8080/api/flight/buy");;
+            .setHeader("id", header(Exchange.TIMER_COUNTER))
+            .setHeader(Exchange.HTTP_METHOD, constant("POST"))
+            .log("Executing saga #${header.id}")
+            .to("http4://localhost:9081/medewerkers?httpMethod=GET")
+            .to("http4://localhost:8080/api/flight/buy");
     }
 
 }
