a9f8ab04f5f2baf6cee9ebb380a97edd50a92e3c
coder-x-esb <serkan.eskici@stedin.net>
2019-09-10 17:24:49 +0200

    added Jaeger config

---
+++ saga-orchestrator/src/main/java/net/stedin/demo/saga/orchestrator/JaegerConfig.java
@@ -0,0 +1,27 @@
+package net.stedin.demo.saga.orchestrator;
+
+import io.jaegertracing.Configuration;
+import io.jaegertracing.Configuration.ReporterConfiguration;
+import io.jaegertracing.Configuration.SamplerConfiguration;
+import io.jaegertracing.Configuration.SenderConfiguration;
+import io.opentracing.Tracer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+@org.springframework.context.annotation.Configuration
+public class JaegerConfig {
+
+    @Value("camel.springboot.name")
+    private String appName;
+
+    @Value("opentracing.jaeger.http-sender.url")
+    private String jeagerUrl;
+
+    @Bean
+    public Tracer getTracer() {
+        return new Configuration(this.appName)
+            .withSampler(new SamplerConfiguration().withType("const").withParam(1))
+            .withReporter(new ReporterConfiguration().withSender(SenderConfiguration.fromEnv().withEndpoint(this.jeagerUrl)))
+            .getTracer();
+    }
+}
--- saga-orchestrator/src/main/java/net/stedin/demo/saga/orchestrator/MySpringBootApplication.java
+++ saga-orchestrator/src/main/java/net/stedin/demo/saga/orchestrator/MySpringBootApplication.java
@@ -1,8 +1,10 @@
 package net.stedin.demo.saga.orchestrator;
 
+import org.apache.camel.opentracing.starter.CamelOpenTracing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@CamelOpenTracing
 @SpringBootApplication
 public class MySpringBootApplication {
 
